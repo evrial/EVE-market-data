4cb60f230c9989c8084f8de42565639fbb819985
Alex Grigoryev <evrial@gmail.com>
2017-02-27 16:38:38 +0200

    Update

--- esi_markethistory.py
+++ esi_markethistory.py
@@ -1,9 +1,9 @@
 """
 https://esi.tech.ccp.is/
 https://developers.eveonline.com/resource/resources
-Download CSV tables from the latest Static Data Export:
-https://www.fuzzwork.co.uk/dump/latest/
-https://stackoverflow.com/questions/40417503/applying-retry-on-grequests-in-python
+Download 2 CSV tables from the latest Static Data Export:
+https://www.fuzzwork.co.uk/dump/latest/mapRegions.csv.bz2
+https://www.fuzzwork.co.uk/dump/latest/invTypes.csv.bz2
 """
 import csv
 import gzip
@@ -15,7 +15,7 @@ from requests.adapters import HTTPAdapter
 from requests.packages.urllib3.util.retry import Retry
 from raven.handlers.logging import SentryHandler
 from raven.conf import setup_logging
-from tqdm import tqdm
+from tqdm import tnrange, tqdm_notebook
 
 
 logger = logging.getLogger(__name__)
@@ -23,22 +23,20 @@ handler = SentryHandler('https://d4e61a4be1164e709412a0e826daa3d8:a37e2bc8875349
 setup_logging(handler)
 
 def get_data(session, type_id, region_id):
-    url = 'https://esi.tech.ccp.is/latest/markets/{region_id}/history/'.format(
-        region_id=region_id)
+    url = 'https://esi.tech.ccp.is/latest/markets/{}/history/'.format(region_id)
     params = {'type_id': type_id, 'datasource': 'tranquility'}
     future = session.get(url, params=params, timeout=30)
     future.type_id = type_id
     future.region_id = region_id
     return future
 
-def process_data(result):
+def process_data(resp):
     try:
-        r = result.result()
+        r = resp.result()
         r.raise_for_status()
         assert r.status_code == 200
         for day in r.json():
-            day['type_id'] = result.type_id
-            day['region_id'] = result.region_id
+            day.update({'type_id': resp.type_id, 'region_id': resp.region_id})
             writer.writerow(day)
     except Exception as e:
         logger.exception(e)
@@ -47,26 +45,26 @@ def process_data(result):
 if __name__ == '__main__':
     session = FuturesSession(max_workers=50)
     session.headers.update({'UserAgent': 'Fuzzwork Market Monitor'})
-
-    retries = Retry(total=50, backoff_factor=0.2, status_forcelist=[500, 502, 503, 504],
+    # https://stackoverflow.com/questions/40417503/applying-retry-on-grequests-in-python
+    retries = Retry(total=50, backoff_factor=0.5, status_forcelist=[500, 502, 503, 504],
         raise_on_redirect=True, raise_on_status=True)
     session.mount('http://', HTTPAdapter(max_retries=retries, pool_maxsize=50))
     session.mount('https://', HTTPAdapter(max_retries=retries, pool_maxsize=50))
 
-    regions = pd.read_csv('mapRegions.csv.bz2', usecols=['regionID'])
-    types = pd.read_csv('invTypes.csv.bz2', usecols=['typeID', 'published', 'marketGroupID'])
-    types = types[(types.published == 1) & (types.marketGroupID != 'None')]
+    regions = pd.read_csv('data/mapRegions.csv.bz2')
+    types = pd.read_csv('data/invTypes.csv.bz2', usecols=['typeID', 'published', 'marketGroupID'])
+    types = types.query('published == 1 and marketGroupID != "None"')
 
-    with gzip.open('history-latest.csv.gz', 'w') as csvfile:
+    with gzip.open('data/history-latest.csv.gz', 'wt') as csvfile:
         fieldnames = ['type_id', 'region_id', 'date', 'lowest', 'highest',
                       'average', 'volume', 'order_count']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
         writer.writeheader()
 
-        for region_id in regions['regionID']:
-            futures = (get_data(session, type_id, region_id) for type_id in types['typeID'])
+        for _, row in tqdm_notebook(regions.iterrows(), total=len(regions), desc='Total'):
+            futures = (get_data(session, type_id, row['regionID'])
+                for type_id in types['typeID'])
 
-            for result in tqdm(as_completed(futures),
-                    desc='Downloading region {}'.format(region_id),
-                    total=len(types), leave=True):
-                process_data(result)
+            for resp in tqdm_notebook(as_completed(futures), total=len(types), leave=False,
+                    unit='req', desc='{}'.format(row['regionName'])):
+                process_data(resp)
---
+++ ws_server.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# https://websockets.readthedocs.io/en/stable/intro.html#basic-example
+
+import asyncio
+import websockets
+
+async def hello(websocket, path):
+    name = await websocket.recv()
+    print("< {}".format(name))
+
+    greeting = "Hello {}!".format(name)
+    await websocket.send(greeting)
+    print("> {}".format(greeting))
+
+start_server = websockets.serve(hello, 'localhost', 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
